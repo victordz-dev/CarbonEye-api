485cde3b2c5ec7027342180ea606debbd02c1339
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2026-06-09 17:42:00 -0300

    habilitando cors para permitir requisições em ambiente de desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.use(helmet());
   app.useGlobalPipes(new ZodValidationPipe());
